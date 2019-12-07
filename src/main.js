ba3a343cbbb69f4ec01225de69c86c63de9a8466
x3mproskared <56311655+x3mproskared@users.noreply.github.com>
2019-12-07 10:23:46 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,14 +10,14 @@ import vuetify from './plugins/vuetify';
 Vue.use(Vuetify)
 
 firebase.initializeApp({
-  apiKey: 'AIzaSyCUNUPbTWov6URfHmMxm_qk80jz8rUgqWw',
-  authDomain: 'law-agency.firebaseapp.com',
-  databaseURL: 'https://law-agency.firebaseio.com',
-  projectId: 'law-agency',
-  storageBucket: 'law-agency.appspot.com',
-  messagingSenderId: '878265870283',
-  appId: '1:878265870283:web:f64c9c444d06bcb321784e',
-  measurementId: 'G-PG2JCLG9Y7'
+  apiKey: 'FUCK.OFF.EVIL.PEDRILA.OR.I.WILL.SHOT.U.IN.YOUR.FACE.BREAK.THE.LEGS.YOURN.NOSE.BEFORE',
+  authDomain: 'FUCK.OFF.EVIL.PEDRILA.OR.I.WILL.SHOT.U.IN.YOUR.FACE.BREAK.THE.LEGS.YOURN.NOSE.BEFORE',
+  databaseURL: 'FUCK.OFF.EVIL.PEDRILA.OR.I.WILL.SHOT.U.IN.YOUR.FACE.BREAK.THE.LEGS.YOURN.NOSE.BEFORE',
+  projectId: 'FUCK.OFF.EVIL.PEDRILA.OR.I.WILL.SHOT.U.IN.YOUR.FACE.BREAK.THE.LEGS.YOURN.NOSE.BEFORE',
+  storageBucket: 'FUCK.OFF.EVIL.PEDRILA.OR.I.WILL.SHOT.U.IN.YOUR.FACE.BREAK.THE.LEGS.YOURN.NOSE.BEFORE',
+  messagingSenderId: 'FUCK.OFF.EVIL.PEDRILA.OR.I.WILL.SHOT.U.IN.YOUR.FACE.BREAK.THE.LEGS.YOURN.NOSE.BEFORE',
+  appId: 'FUCK.OFF.EVIL.PEDRILA.OR.I.WILL.SHOT.U.IN.YOUR.FACE.BREAK.THE.LEGS.YOURN.NOSE.BEFORE',
+  measurementId: 'FUCK.OFF.EVIL.PEDRILA.OR.I.WILL.SHOT.U.IN.YOUR.FACE.BREAK.THE.LEGS.YOURN.NOSE.BEFORE',
 })
 
 Vue.config.productionTip = false
